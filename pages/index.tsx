cb1bfd9b7af49678afff47a8fa7f6d50e1d92fd2
Marcuz Corpuz <macoycorpuz@gmail.com>
2022-11-14 16:48:34 +0900

    initialize mapbox

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,48 @@
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import type { NextPage } from 'next';
+import { useEffect, useRef, useState } from 'react';
+
+const Map: NextPage = () => {
+  const mapContainer = useRef<any>(null);
+  const map = useRef<mapboxgl.Map | any>(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN ?? '';
+
+  useEffect(() => {
+    if (map.current || !mapboxgl.accessToken) return;
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom,
+    });
+  });
+
+  useEffect(() => {
+    if (!map.current || !mapboxgl.accessToken) return;
+    map.current.on('move', () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
+
+  if (!mapboxgl.accessToken) {
+    return <div>You don&apos;t have access to mapbox</div>;
+  }
+
+  return (
+    <main className="w-screen h-screen">
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="w-full h-full" />
+    </main>
+  );
+};
+
+export default Map;
