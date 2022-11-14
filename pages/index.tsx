82f46879999e08685c0c77eaa854cf8f666cbdfb
Marcuz Corpuz <macoycorpuz@gmail.com>
2022-11-14 17:21:35 +0900

    add controls and change ico

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,42 @@
-import mapboxgl from 'mapbox-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
 import type { NextPage } from 'next';
 import { useEffect, useRef, useState } from 'react';
+
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 const Map: NextPage = () => {
   const mapContainer = useRef<any>(null);
   const map = useRef<mapboxgl.Map | any>(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
+  const [lng, setLng] = useState(120.8114);
+  const [lat, setLat] = useState(14.8437);
+  const [zoom, setZoom] = useState(10);
 
   mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN ?? '';
 
   useEffect(() => {
     if (map.current || !mapboxgl.accessToken) return;
+
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [lng, lat],
       zoom: zoom,
     });
+
+    const geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl,
+    });
+    const nav = new mapboxgl.NavigationControl();
+
+    map.current.addControl(geocoder);
+    map.current.addControl(nav);
   });
 
   useEffect(() => {
-    if (!map.current || !mapboxgl.accessToken) return;
+    if (!map.current) return;
     map.current.on('move', () => {
       setLng(map.current.getCenter().lng.toFixed(4));
       setLat(map.current.getCenter().lat.toFixed(4));
