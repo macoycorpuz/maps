97ca71d02dbaebcc80febe36b255a48a70c09570
Marcuz Corpuz <macoycorpuz@gmail.com>
2022-12-05 23:53:41 +0900

    general metadata

---
+++ src/data/general.ts
@@ -0,0 +1,49 @@
+export const general = [
+  {
+    id: 'PH031400000',
+
+    name: 'BULACAN',
+    metadata: [
+      {
+        name: 'Region',
+        value: 'REGION III (CENTRAL LUZON)',
+        icon: 'MapPinIcon',
+      },
+      {
+        name: 'Region Code',
+        value: 'PH030000000',
+        icon: 'CodeBracketSquareIcon',
+      },
+      {
+        name: 'Land Area',
+        value: '278,369 has.',
+        icon: 'MapPinIcon',
+      },
+      {
+        name: 'Population',
+        value: '3,708,890.00',
+        icon: 'UserGroupIcon',
+      },
+      {
+        name: 'Number of Municipalities',
+        value: '24 (3 Cities)',
+        icon: 'BuildingOfficeIcon',
+      },
+      {
+        name: 'Number of Baranggays',
+        value: 569,
+        icon: 'BuildingOffice2Icon',
+      },
+      {
+        name: 'Governor',
+        value: 'Hon. Daniel R. Fernando',
+        icon: 'UserIcon',
+      },
+      {
+        name: 'Vice Governor',
+        value: 'Hon. Alex C. Castro',
+        icon: 'UserIcon',
+      },
+    ],
+  },
+];
